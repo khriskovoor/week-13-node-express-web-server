9e16b89330f49d681c9d31ff5b3664c70b5a4eb5
Khris Kovoor <Khris Kovoor>
2016-09-02 23:53:41 -0400

    submit not working

---
+++ 13-week-node-express-web-server/friendFinder/app/data/friends.js
@@ -0,0 +1,44 @@
+
+var friends = [
+{
+
+   		name: "Fry ",
+		photo: "http://images6.fanpop.com/image/photos/34200000/more-dumb-images-of-philip-j-fry-futurama-34257099-500-313.png",
+		scores: [3, 4, 5, 5, 2, 1, 2, 3, 4, 5]	
+	},
+
+	{
+		name: "Leela ",
+		photo: "http://images2.fanpop.com/image/photos/8900000/Leela-turanga-leela-8997933-429-350.jpg",
+		scores: [1, 2, 3, 4, 5, 1, 4, 3, 5, 2]	
+	},
+
+	{
+		name: "Bender ",
+		photo: "http://orig02.deviantart.net/0ac6/f/2008/222/f/0/bender_by_onebitrocket.jpg",
+		scores: [1, 2, 3, 4, 5, 2, 2, 3, 4, 5]	
+	},
+
+	{
+		name: "Zapp Brannigan ",
+		photo: "https://67.media.tumblr.com/3f5a3f70aeca98eea2ba8d1f49ea12bc/tumblr_mkwdnu88dO1r8q9x8o1_500.gif",
+		scores: [1, 1, 2, 4, 5, 2, 1, 3, 5, 2]	
+	},
+
+
+	{
+		name: "Calculon ",
+		photo: "http://vignette3.wikia.nocookie.net/en.futurama/images/b/b3/Calculon-2.JPG/revision/latest?cb=20090604015557",
+		scores: [3, 3, 2, 4, 5, 5, 1, 1, 1, 1]	
+	},
+
+
+	{
+		name: "Zoidberg ",
+		photo: "http://img15.deviantart.net/4f37/i/2012/037/1/5/zoidberg____by_boneswolbach-d4ovq6d.png",
+		scores: [2, 1, 1, 4, 5, 5, 5, 5, 5, 1]	
+	}
+
+]
+
+module.exports = friends;
